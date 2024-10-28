be36f99cce9db2a05bb12cc733882b379413bef6
georgeluobe007 <georgedaninno@gmail.com>
2024-10-28 10:22:45 +0100

    second change

--- src/Components/FirstPage.js
+++ src/Components/FirstPage.js
@@ -1,5 +1,8 @@
 import React,{useState} from 'react'
 import '../CSSComponent/Page.css'
+import { Icon } from 'react-icons-kit'
+import {ic_keyboard_arrow_up} from 'react-icons-kit/md/ic_keyboard_arrow_up'
+import {ic_keyboard_arrow_down_twotone} from 'react-icons-kit/md/ic_keyboard_arrow_down_twotone'
 function FirstPage() {
 const [myPicture,setMyPicture] = useState(false)
 const [objective1,setObjective1] = useState(false)
@@ -50,18 +53,24 @@ const [myReferees,setMyReferees] = useState(false)
        </div>
         <div className='heading'>
         <h3 className='my_name'> IBHALUOBE GODWIN GEORGE </h3>
-        <h3 className='abot_me'> fullStack web application(React.js,typeScript node.js, mySql and MongooseDB) </h3>
+        <h3 className='abot_me'> fullStack web application(React.js,react-native(Mobile), typeScript, node.js, mySql and MongooseDB) </h3>
         <h4 className='my_address'>55a, Rasaki Oladega Street, Satellite Town Lagos.</h4>
         <h4 className='my_phone'>Phone: 07069183891</h4>
         </div>
-        <h3 className='objective' onClick={objective}>OBJECTIVES:</h3>
+        <h3 className='objective' onClick={objective}>OBJECTIVES
+         {objective1 === false ? (<Icon icon={ic_keyboard_arrow_down_twotone} style={{width:"3rem"}}/>):
+         (<Icon icon={ic_keyboard_arrow_up}  style={{width:"3rem"}} />)}
+         </h3>
         {objective1 && <div className='objective1'>
           <p>To work in a dynamic environment where challenges 
             can be turned into development and professional expertise, 
             with a positive oriented organization structure for the achievement of
              the peak goals of the organization and individuals.</p>
         </div> }
-        <h3 className='biodata'onClick={biodata} >BIO DATA:</h3>
+        <h3 className='biodata'onClick={biodata} >BIO DATA
+        {information === false ? (<Icon icon={ic_keyboard_arrow_down_twotone} style={{width:"3rem"}}/>):
+         (<Icon icon={ic_keyboard_arrow_up}  style={{width:"3rem"}} />)}
+        </h3>
         {information && <div className='biodata1'>
           <table>
           <tr className='right-td'>
@@ -86,32 +95,47 @@ const [myReferees,setMyReferees] = useState(false)
           </tr>
           </table>
           </div>}
-          <h3 className='school'onClick={school} >EDUCATIONAL INSTITUTION:</h3>
+          <h3 className='school'onClick={school} >EDUCATIONAL INSTITUTION
+          {institution === false ? (<Icon icon={ic_keyboard_arrow_down_twotone} style={{width:"3rem"}}/>):
+         (<Icon icon={ic_keyboard_arrow_up}  style={{width:"3rem"}} />)}
+          </h3>
           {institution && <div className='mylist'>
           <ul>
           <li className='school_attends'>Gaskiya College, Ijora Badia, Lagos.</li>
            <li className='school_attends'>Autonet Computer Institution</li>
           </ul>
             </div>}
-            <h3 className='qualification'onClick={qualification} >QUALIFICATIONS:</h3>
+            <h3 className='qualification'onClick={qualification} >QUALIFICATIONS
+            {myqualification === false ? (<Icon icon={ic_keyboard_arrow_down_twotone} style={{width:"3rem"}}/>):
+            (<Icon icon={ic_keyboard_arrow_up}  style={{width:"3rem"}} />)}
+            </h3>
             {myqualification && <div className='mylist'>
               <ul>
               <li className='school_attends'>Computer programming.</li>
               <li className='school_attends'>First School Leaving Certificate </li>
              </ul>
               </div>}
-              <h3 className='workExpirience'onClick={workExpirience} >WORK EXPERIENCE:</h3>
+              <h3 className='workExpirience'onClick={workExpirience} >WORK EXPERIENCE
+              {myWorkExpirience === false ? (<Icon icon={ic_keyboard_arrow_down_twotone} style={{width:"3rem"}}/>):
+              (<Icon icon={ic_keyboard_arrow_up}  style={{width:"3rem"}} />)}
+              </h3>
               {myWorkExpirience && <div className='mylist'>
                 <ul>
                 <li className='school_attends'>Tanikem Computer School and Data Management .</li>
                 <li className='school_attends'>Work from home as a freelancer</li>
                 </ul>
                 </div>}
-                <h3 className='hobbies'onClick={hobbies} >HOBBIES:</h3>
-                {myhobbies && <div className='mylist'>
+                <h3 className='hobbies'onClick={hobbies} >HOBBIES
+                  {myhobbies === false ? (<Icon icon={ic_keyboard_arrow_down_twotone} style={{width:"3rem"}}/>):
+                 (<Icon icon={ic_keyboard_arrow_up}  style={{width:"3rem"}} />)}
+                </h3>
+                  {myhobbies && <div className='mylist'>
                   <h3 className='school_attends'>Reading and Driving</h3>
                   </div>}
-                  <h3 className='referees'onClick={referees} >REFEREES:</h3>
+                  <h3 className='referees'onClick={referees} >REFEREES
+                  {myReferees === false ? (<Icon icon={ic_keyboard_arrow_down_twotone} style={{width:"3rem"}}/>):
+                   (<Icon icon={ic_keyboard_arrow_up}  style={{width:"3rem"}} />)}
+                  </h3>
                   {myReferees && <div className='mylist'>
                    <p>On Request</p>
                   </div>}
